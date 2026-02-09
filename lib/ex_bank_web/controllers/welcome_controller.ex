e67d51e83ac55b4168051504b4d328da2b3133a1
Sam Marxz <sammarxzz@gmail.com>
2026-02-09 19:48:23 -0300

    create "users" migration

--- lib/ex_bank_web/controllers/welcome_controller.ex
+++ lib/ex_bank_web/controllers/welcome_controller.ex
@@ -2,6 +2,8 @@ defmodule ExBankWeb.WelcomeController do
   use ExBankWeb, :controller
 
   def index(conn, _params) do
-    send_resp(conn, 200, "Hello World")
+    conn
+    |> put_status(200)
+    |> json(%{message: "Welcome to ExBank"})
   end
 end
