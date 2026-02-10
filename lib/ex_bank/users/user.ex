c71eed9394563c132b65d4edc46271fe61d4f376
Sam Marxz <sammarxzz@gmail.com>
2026-02-10 10:42:27 -0300

    feat: Implement email format and uniqueness validations, refactor required fields into a constant, and adjust CEP length validation in the user changeset.

--- lib/ex_bank/users/user.ex
+++ lib/ex_bank/users/user.ex
@@ -2,6 +2,8 @@ defmodule ExBank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @required_params [:name, :email, :password_hash, :cep]
+
   schema "users" do
     field :name, :string
     field :email, :string
@@ -13,11 +15,13 @@ defmodule ExBank.Users.User do
 
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :password_hash, :cep])
-    |> validate_required([:name, :email, :password_hash, :cep])
+    |> cast(attrs, @required_params)
+    |> validate_required(@required_params)
     |> validate_length(:name, min: 3)
     |> validate_length(:email, min: 3)
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+\.[^\s]+$/)
+    |> unique_constraint(:email)
     |> validate_length(:password_hash, min: 6)
-    |> validate_length(:cep, min: 8)
+    |> validate_length(:cep, is: 8)
   end
 end
